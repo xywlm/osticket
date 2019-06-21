253017f7d94c32f2601b7f6480b705b88b27d2e0
xywlm <35143766+xywlm@users.noreply.github.com>
2019-06-21 14:49:05 +0800

    Update ost-config.php

--- include/ost-config.php
+++ include/ost-config.php
@@ -39,9 +39,9 @@ define('ADMIN_EMAIL','demouser@opsgility.com');
 # ---------------------------------------------------
 # Mysql Login info
 define('DBTYPE','mysql');
-define('DBHOST','yourservername.mysql.database.azure.com');
+define('DBHOST','xymysql.mysql.database.azure.com');
 define('DBNAME','osticket');
-define('DBUSER','demouser@yourservername');
+define('DBUSER','demouser@xymysql');
 define('DBPASS','demo@pass123');
 
 # Table prefix
